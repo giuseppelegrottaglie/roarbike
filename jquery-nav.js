c142ba8db609424a19a2bdb5e4291cfd89850e4f
giuseppelegrottaglie <giuseppe.legrottaglie4@gmail.com>
2021-03-09 21:42:50 +0100

    Modifica

---
+++ jquery-nav.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+
+    $('.mob-wrapper').on('click', function(){
+        $('.mob-nav').fadeToggle("fast");
+    })
+
+});
---
+++ scripts.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function(){
+
+    $('.mob-wrapper').on('click', function(){
+        $('.mobile-nav').fadeToggle("fast");
+    })
+
+});
